2a1109629dfbdbc6f4d95fe8abeb456cf5fa9495
notyalc <git.notyalc@gmail.com>
2013-09-30 19:28:30 -0500

    learning socket.io changes

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ app.get('/', function (req, res) {
   res.render('testJavascript.jade');
 });
 
+
+
+var socks = [];
 io.sockets.on('connection', function (socket) {
-  socket.on('entered', function (submitted) {
-    console.log(submitted);
-  });
+ 	socks.push(socket);
+console.log('There are currently ' + socks.length + 'sockets stored in socks and this is the array of socks: ' + socks);
+if(socks.length==2){
+		 console.log('this is the socket to be used');
+		 socks[0].on('entered', function(words){
+		 	socks[0].emit('sent', {what: words});
+		 });
+     socks[1].on('entered', function(words){
+      socks[1].emit('sent', {what:words});
+     });
+   }
 });
